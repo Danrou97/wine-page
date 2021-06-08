2726036e7415da55c3f7d2fdb392e917ddd8bb36
Danrou97 <83980134+Danrou97@users.noreply.github.com>
2021-06-07 21:23:16 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-let menu = document.querySelector("#menu")
-let toggleIcon = document.querySelector("#toggle-icon")
-
-toggleIcon.addEventListener("click", function() {
-    menu.classList.toggle("menu-on")
-})
